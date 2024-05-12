1c22503e5ab2ffdbd5f7bd1dd457ee2992dea827
ahmedtarek12343 <131623405+ahmedtarek12343@users.noreply.github.com>
2024-05-12 14:45:56 +0300

    input cases done

--- ProjectMetricsFP/MainMenu.cs
+++ ProjectMetricsFP/MainMenu.cs
@@ -32,6 +32,14 @@ namespace ProjectMetricsFP
                 MessageBox.Show("All not empty");
             }
 
+            bool istcfgiven = true;
+            //If TCF is empty
+
+            if (string.IsNullOrEmpty(tcfTextbox.Text))
+            {
+                istcfgiven = false;
+            }
+
             //If UFP empty
             if (string.IsNullOrEmpty(ufpTextbox.Text))
             {
@@ -44,15 +52,16 @@ namespace ProjectMetricsFP
                     MessageBox.Show(calculateUFP.ufpValue.ToString());
                 }
 
+                if (istcfgiven)
+                {
+                    string val = tcfTextbox.Text.ToString();
+                    float di=(float)((int.Parse(val)-0.65)/0.01);
+                    MessageBox.Show("di value is: "+di);
+                }
                 
             }
             
-            bool istcfgiven = true;
-                //If TCF is empty
-             if (string.IsNullOrEmpty(tcfTextbox.Text))
-            {
-                istcfgiven = false;
-            }
+         
           
 
             //If BOTH DI and TCF are empty
@@ -62,18 +71,21 @@ namespace ProjectMetricsFP
                 DialogResult result = diCalc.ShowDialog();
                 Visible = false;
                 MessageBox.Show(calculateDI.divlaue.ToString());
-                if (!istcfgiven)
-                {
-                    float tcfvalue = (float)(0.65 + 0.01 * calculateDI.divlaue);
-                    MessageBox.Show("tcf value is: " + tcfvalue);
-                }
             }
-
-            if (!istcfgiven)
+            if (istcfgiven && !string.IsNullOrEmpty(ufpTextbox.Text))
             {
-                MessageBox.Show("tcf values wasnt given");
+                string val = tcfTextbox.Text.ToString();
+                float di = (float)((int.Parse(val) - 0.65) / 0.01);
+                MessageBox.Show("di value is: " + di);
             }
 
+            if (!istcfgiven )
+            {
+                float tcfvalue = (float)(0.65 + 0.01 * calculateDI.divlaue);
+                MessageBox.Show("tcf value is: " + tcfvalue);
+
+            }
+    
             Close();
             
         }
